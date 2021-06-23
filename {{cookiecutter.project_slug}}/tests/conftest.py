e6a4298bd4b16c63ad1b227c5efc6524f5dc96c0
André Felipe Dias <andre.dias@pronus.io>
2021-06-23 11:25:27 -0300

    use docker-compose instead of invocating docker containers individually

--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -1,47 +1,25 @@
-import os
-from subprocess import DEVNULL, check_call
-from typing import AsyncIterable, Generator
+"""
+Important
+---------
+
+Tests will only work if service containers are running.
+See Makefile for instructions.
+"""
+
+from typing import AsyncIterable
 
 from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from httpx import AsyncClient
 from pytest import fixture
 
-os.environ['ENV'] = 'testing'
-
-from app.config import DB_NAME, DB_PASSWORD, DB_PORT, REDIS_PORT  # noqa: E402
-from app.main import app as _app  # noqa: E402
-from app.models.user import get_all as get_all_users  # noqa: E402
-from app.schemas.user import UserInfo  # noqa: E402
-
-
-@fixture(scope='session')
-def docker() -> Generator:
-    check_call(
-        f'docker run -d --rm -e POSTGRES_DB={DB_NAME} '
-        f'-e POSTGRES_PASSWORD={DB_PASSWORD} '
-        f'-p {DB_PORT}:5432 --name postgres-testing postgres:alpine',
-        stdout=DEVNULL,
-        shell=True,
-    )
-    check_call(
-        f'docker run -d --rm -p {REDIS_PORT}:6379 '
-        '--name redis-testing redis:alpine',
-        stdout=DEVNULL,
-        shell=True,
-    )
-    try:
-        yield
-    finally:
-        check_call(
-            'docker stop -t 0 postgres-testing redis-testing',
-            stdout=DEVNULL,
-            shell=True,
-        )
+from app.main import app as _app
+from app.models.user import get_all as get_all_users
+from app.schemas.user import UserInfo
 
 
 @fixture
-async def app(docker) -> AsyncIterable[FastAPI]:
+async def app() -> AsyncIterable[FastAPI]:
     async with LifespanManager(_app):
         yield _app
 
